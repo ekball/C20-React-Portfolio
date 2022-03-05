10d77ee975d54a52c07d3a45b49790e1147219c8
ekball <90882418+ekball@users.noreply.github.com>
2022-03-04 22:58:17 -0500

    set up initial file structure

--- src/App.js
+++ src/App.js
@@ -3,22 +3,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+      {/* header section containing nav */}
+      <header className='header'>
+
+        <Nav>
+        </Nav>
+
       </header>
+
+      {/* main section containing About Me, Projects, Resume, and Contact Me */}
+      <main className='main'>
+
+        <About className='about'>
+        </About>
+
+        <Projects className='projects'>
+        </Projects>
+
+        <Resume className='resume'>
+        </Resume>
+
+        <Contact className='contact'>
+        </Contact>
+
+      </main>
+
+      <footer className='footer'>
+
+      </footer>
     </div>
+
   );
 }
 
