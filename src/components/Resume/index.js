6db7dadb62645bc760cca589263dc1d78f92285e
ekball <90882418+ekball@users.noreply.github.com>
2022-04-20 13:24:37 -0400

    added skillset to resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,11 +2,41 @@ import React from "react";
 
 function Resume() {
 
+    const skills = [
+        'React',
+        'Javascript', 
+        'GraphQL',
+        'MERN Stack',
+        'Node.js',
+        'MongoDB',
+        'Express.js',
+        'NoSQL', 
+        'GitHub', 
+        'CSS', 
+        'DRY Methods',
+        'OOP', 
+        'MVC Paradigm',
+        'Progressive Web Applications',
+    ]
+
+
     return(
         <div className="resume-section">
-            <h1 className="resume-title">Resume</h1>
+            <div>
+                <h1 className="resume-title">Resume</h1>
 
-            <a href='../assets/docs/resume.pdf' download className="resume-link">Click here to download a copy of my resume</a>
+                <a href='../assets/docs/resume.pdf' download className="resume-link">Click here to download a copy of my resume</a>
+            </div>
+
+            <div>
+                <h1>Skills</h1>
+
+                <ul>
+                        {skills.map(skill => {
+                            return <li>{skill}</li>
+                        })}
+                </ul>
+            </div>
         </div>
     );
 }
